5a603115fa831d2e113221070296ed982d99aefd
bioinformatist <sun_yu@mail.nankai.edu.cn>
2016-04-30 14:02:51 +0800

    A crawler for Wu's work

---
+++ main.py
@@ -0,0 +1,14 @@
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+url = 'http://rice.plantbiology.msu.edu/annotation_community_families.shtml'
+html = urlopen(url)
+# print(html.read().decode(encoding='utf8'))
+bs_obj = BeautifulSoup(html, 'html.parser')
+# print(bs_obj.prettify())
+# print(bs_obj.title)
+# print(bs_obj.h1.get_text())
+
+for link in bs_obj.find_all('a'):
+    print(link)
