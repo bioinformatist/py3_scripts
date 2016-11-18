0ebc076dc5e581efb10e5e517d435191e9988581
Yu Sun <sun_yu@mail.nankai.edu.cn>
2016-11-18 11:12:55 +0800

    remove abundant, add comments

--- pcbXmlSupplier.py
+++ pcbXmlSupplier.py
@@ -9,13 +9,14 @@ import shutil
 
 
 def xml_copy(path):
+    '''
+    To get xml file names and copy them into proper directories separately.
+    '''
     pattern = re.compile("cell\d+")
     dir_list = os.listdir(path)
     for dir in dir_list:
         if pattern.match(dir):
             os.chdir(dir + "/Analysis_Results")
-            print(os.listdir(".")[0].split(".")[0])
-            print("../../meta/" + os.listdir(".")[0].split(".")[0] + ".metadata.xml")
             shutil.copy("../../meta/" + os.listdir(".")[0].split(".")[0] + ".metadata.xml", "..")
             os.chdir("../..")
 
