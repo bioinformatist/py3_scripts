73409b1e48100771cc9f6e03a71809e1f2a4dd10
Yu Sun <sun_yu@mail.nankai.edu.cn>
2016-11-23 17:11:39 +0800

    added situation without correct dir structure

--- pcbXmlSupplier.py
+++ pcbXmlSupplier.py
@@ -12,11 +12,21 @@ def xml_copy(path):
     '''
     To get xml file names and copy them into proper directories separately.
     '''
-    pattern = re.compile("cell\d+")
-    dir_list = os.listdir(path)
-    for dir in dir_list:
-        if pattern.match(dir):
-            os.chdir(dir + "/Analysis_Results")
+    cell_name_pattern = re.compile("cell\d+")
+    cell_dir_list = os.listdir(path)
+    for dir in cell_dir_list:
+        if cell_name_pattern.match(dir):
+            try:
+                os.chdir(dir + os.sep + "Analysis_Results")
+            except:
+                os.chdir(dir)
+                os.mkdir("Analysis_Results")
+                h5_file_list = os.listdir(".")
+                for h5_file in h5_file_list:
+                    if h5_file.endswith(".bax.h5"):
+                        print("Performing on file:", h5_file, "...")
+                        shutil.move(h5_file, "Analysis_Results")
+                os.chdir("Analysis_Results")
             shutil.copy("../../meta/" + os.listdir(".")[0].split(".")[0] + ".metadata.xml", "..")
             os.chdir("../..")
 
